262879f6c0799ed7fd8366d2b4edf26c50807fb7
JESUS RAMOS <jeramauni@gmail.com>
2019-03-02 00:50:06 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ GameJam2019/Assets/Scripts/Gun.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class Gun : MonoBehaviour {
+
+   /* public float damage = 10.0f;
+    public float range = 100.0f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public AudioSource shootSFX;
+
+    void Start()
+    {
+        shootSFX = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        shootSFX.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }*/
+}
