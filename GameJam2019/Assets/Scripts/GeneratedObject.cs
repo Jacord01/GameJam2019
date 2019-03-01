297fe5243762125f95021fc185c8211d935438c5
Javier Cordero <jacord01@ucm.es>
2019-03-01 22:13:57 +0100

    mazo coshas

--- GameJam2019/Assets/Scripts/GeneratedObject.cs
+++ GameJam2019/Assets/Scripts/GeneratedObject.cs
@@ -2,7 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneratedObject : MonoBehaviour {
+public class GeneratedObject : MonoBehaviour
+{
+	bool canIBeDestroyed = false;
+	
+	bool playerInsideMe = false;
 
 	
+
+	private void Start()
+	{
+		int rnd = Random.Range(0,4);
+
+		if(rnd == 0)
+		{
+			canIBeDestroyed = true;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			FindObjectOfType<ActivateTextPush>().showText();
+
+			playerInsideMe = true;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E) && playerInsideMe && canIBeDestroyed)
+		{
+			FindObjectOfType<ActivateTextPush>().hideText();
+
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			FindObjectOfType<ActivateTextPush>().hideText();
+
+			playerInsideMe = false;
+
+		}
+	}
+
 }
