5faa069728af69a6e54abd0b57edbed1b9a26735
DPerezLuque <daniep04@ucm.es>
2019-03-02 06:59:59 +0100

    Los enemigos espawnean fragmentos

---
+++ GameJam2019/Assets/Scripts/FragmentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // SUMAR PUNTOS
+            Destroy(gameObject);
+        }
+    }
+}
--- GameJam2019/Assets/Scripts/Target.cs
+++ GameJam2019/Assets/Scripts/Target.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour {
 
     public float health = 100.0f;
+    public GameObject prize;
 
     public void TakeDamage(float dmg)
     {
@@ -12,8 +13,8 @@ public class Target : MonoBehaviour {
 
         if (health <= 0.0f)
         {
+            Instantiate(prize, transform.position + new Vector3(0, 1, 0), transform.rotation);
             Transform o = transform.parent;
-
             if (o != null) Destroy(o.gameObject);
             else Destroy(gameObject);
         }
