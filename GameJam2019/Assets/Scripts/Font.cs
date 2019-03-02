3be5767d0519f1b403a6cbdb35908f8d019340ab
JESUS RAMOS <jeramauni@gmail.com>
2019-03-02 03:28:10 +0100

    Errors Fixed

--- GameJam2019/Assets/Scripts/Font.cs
+++ GameJam2019/Assets/Scripts/Font.cs
@@ -12,31 +12,25 @@ public class Font : MonoBehaviour {
     GameManagerComp gameManager_;
 
     void Start()
-    {      
+    {
+        gameManager_ = GameObject.FindObjectOfType<GameManagerComp>();
         dash_ = false;
         shield_ = false;
         lifeSteal_ = false;
 
+        if (entitiesMargin_ == 0)
+            entitiesMargin_ = 3;
+
         //Cooldown 
-        timeLeft_ = 30.0f;
-        wait_ = true;  
-
-        int rnd = Random.Range(1, 4);
-        currentPower_ = rnd;
-
-        if (rnd == 1)
-            dash_ = true;
-        else if (rnd == 2)
-            shield_ = true;
-        else if (rnd == 3)
-            lifeSteal_ = true;
+        timeLeft_ = 0.0f;
+        wait_ = false;
     }
 
     void Update()
     {
         if (!wait_)
         {
-            timeLeft_ -= Time.deltaTime;           
+            timeLeft_ -= Time.deltaTime;
             if (timeLeft_ < 0)
             {
                 wait_ = true;
@@ -45,7 +39,7 @@ public class Font : MonoBehaviour {
         }
     }
 
-     void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
 	{
 		if (other.GetComponent<GeneratedObject>())
 		{
@@ -73,12 +67,30 @@ public class Font : MonoBehaviour {
             }
         }
     }
+    int CheckMostCommon()
+    {
+        int mostCommon = currentPower_;
+        int fly = 0, slime = 0, robot = 0;
+
+        fly = gameManager_.flyEnemies_.Count;
+        slime = gameManager_.slimeEnemies_.Count;
+        robot = gameManager_.robotEnemies_.Count;
+
+        int x = max(fly, slime);
+        if (max(x, robot) == x)
+            if ((x - robot) >= entitiesMargin_)
+                mostCommon = x;
+            else;
+
+        else if ((robot - x) >= entitiesMargin_)
+            mostCommon = robot;
+
+        return mostCommon;
+    }
 
     public void NextPower()
     {
-        currentPower_ = (currentPower_ + 1) % 4;
-        if (currentPower_ == 0)
-            currentPower_++;
+        currentPower_ = CheckMostCommon();
 
         switch (currentPower_)
         {
@@ -111,76 +123,3 @@ public class Font : MonoBehaviour {
     }//Returns the max between 2 integers,if both are the same, it returns the second one
 
 }
-
-    void Start()
-    {
-        gameManager_ = GameObject.FindObjectOfType<GameManagerComp>();
-        dash_ = false;
-        shield_ = false;
-        lifeSteal_ = false;
-
-        if (entitiesMargin_ == 0)
-            entitiesMargin_ = 3;
-
-        //Cooldown 
-        timeLeft_ = 0.0f;
-        wait_ = false;  
-    }
-
-    void Update()
-    {
-        if (!wait_)
-        {
-            timeLeft_ -= Time.deltaTime;           
-            if (timeLeft_ < 0)
-            {
-                wait_ = true;
-                timeLeft_ = 30.0f;
-            }
-        }
-    }
-
-    int CheckMostCommon()
-    {
-        int mostCommon = currentPower_;
-        int fly = 0, slime = 0, robot = 0;
-
-        fly = gameManager_.flyEnemies_.Count;
-        slime = gameManager_.slimeEnemies_.Count;
-        robot = gameManager_.robotEnemies_.Count;
-
-        int x = max(fly, slime);
-        if (max(x, robot) == x)
-            if ((x - robot) >= entitiesMargin_)
-                mostCommon = x;
-            else;
-
-        else if ((robot - x) >= entitiesMargin_)
-            mostCommon = robot;
-
-            return mostCommon;
-    }
-
-    public void NextPower()
-    {
-        currentPower_ = CheckMostCommon();
-
-        switch (currentPower_)
-        {
-            case 1:
-                dash_ = true;
-                shield_ = false;
-                lifeSteal_ = false;
-                break;
-            case 2:
-                dash_ = false;
-                shield_ = true;
-                lifeSteal_ = false;
-                break;
-            case 3:
-                dash_ = false;
-                shield_ = false;
-                lifeSteal_ = true;
-                break;
-        }
-        wait_ = false;
