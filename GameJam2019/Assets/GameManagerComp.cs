482df42e7e9774aa1c83b4f4d911b2d68447e10a
JESUS RAMOS <jeramauni@gmail.com>
2019-03-02 03:08:56 +0100

    Fuente Arreglada

---
+++ GameJam2019/Assets/GameManagerComp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerComp : MonoBehaviour {
+    public List<GameObject> flyEnemies_ = new List<GameObject>();
+    public List<GameObject> slimeEnemies_ = new List<GameObject>();
+    public List<GameObject> robotEnemies_ = new List<GameObject>();
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
