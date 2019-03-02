a3eac2123a8f3c89770139c5c9c1d3f1cf5c6c53
JESUS RAMOS <jeramauni@gmail.com>
2019-03-02 02:14:13 +0100

    Auto stash before merge of "master" and "origin/master"

--- GameJam2019/Assets/Scripts/Font.cs
+++ GameJam2019/Assets/Scripts/Font.cs
@@ -4,12 +4,101 @@ using UnityEngine;
 
 public class Font : MonoBehaviour {
 
-	private void OnTriggerEnter(Collider other)
+    private bool dash_, shield_, lifeSteal_;
+    private int currentPower_;
+    private float timeLeft_;
+    private bool wait_;
+
+    void Start()
+    {      
+        dash_ = false;
+        shield_ = false;
+        lifeSteal_ = false;
+
+        //Cooldown 
+        timeLeft_ = 30.0f;
+        wait_ = true;  
+
+        int rnd = Random.Range(1, 4);
+        currentPower_ = rnd;
+
+        if (rnd == 1)
+            dash_ = true;
+        else if (rnd == 2)
+            shield_ = true;
+        else if (rnd == 3)
+            lifeSteal_ = true;
+    }
+
+    void Update()
+    {
+        if (!wait_)
+        {
+            timeLeft_ -= Time.deltaTime;           
+            if (timeLeft_ < 0)
+            {
+                wait_ = true;
+                timeLeft_ = 30.0f;
+            }
+        }
+    }
+
+     void OnTriggerEnter(Collider other)
 	{
 		if (other.GetComponent<GeneratedObject>())
 		{
 			Destroy(other.gameObject);
 
 		}
+
+        //Habilidades
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (wait_)
+            {
+                NextPower();
+            }
+        }
 	}
+
+    void OnTriggerStay (Collider other)
+    {
+        //Habilidades
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (wait_)
+            {
+                NextPower();
+            }
+        }
+    }
+
+    public void NextPower()
+    {
+        currentPower_ = (currentPower_ + 1) % 4;
+        if (currentPower_ == 0)
+            currentPower_++;
+
+        switch (currentPower_)
+        {
+            case 1:
+                dash_ = true;
+                shield_ = false;
+                lifeSteal_ = false;
+                break;
+            case 2:
+                dash_ = false;
+                shield_ = true;
+                lifeSteal_ = false;
+                break;
+            case 3:
+                dash_ = false;
+                shield_ = false;
+                lifeSteal_ = true;
+                break;
+        }
+        wait_ = false;
+    }
+
+
 }
