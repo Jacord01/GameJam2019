27f8ba1e9f1875c5a8a8efc001904a7202b38790
javier Cordero Calvo <jacord01@ucm.es>
2019-03-02 04:44:02 +0100

    Particles

--- GameJam2019/Assets/Scripts/Gun.cs
+++ GameJam2019/Assets/Scripts/Gun.cs
@@ -7,6 +7,8 @@ public class Gun : MonoBehaviour {
     public float range = 100.0f;
 
     public Camera fpsCam;
+
+	bool shooting = false;
     //public ParticleSystem muzzleFlash;
     //public GameObject impactEffect;
     //public AudioSource shootSFX;
@@ -19,12 +21,18 @@ public class Gun : MonoBehaviour {
      void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            Debug.Log("Shooting");
             Shoot();
+			shooting = true;
         }
-    }
+
+		else if (Input.GetMouseButtonUp(0))
+		{
+			shooting = false;
+			stopShooting();
+		}
+	}
 
     void Shoot()
     {
@@ -35,12 +43,18 @@ public class Gun : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
+			FindObjectOfType<ShootingAnimation>().shoot();
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
                 target.TakeDamage(damage);
                 //GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            }
-        }
+            }			
+		}
     }
+
+	void stopShooting()
+	{
+		FindObjectOfType<ShootingAnimation>().Noshoot();
+	}
 }
